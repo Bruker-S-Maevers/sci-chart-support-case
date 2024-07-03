653d1cc2b6f709d7bac86ba35a158184a995f399
Maevers, Stephan <S.Maevers@bruker.com>
2024-07-03 11:56:37 +0200

    Initial commit

---
+++ WpfTestApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using SciChart.Charting.Visuals;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Set this code once in App.xaml.cs or application startup
+            // SciChartSurface.SetRuntimeLicenseKey("...");
+        }
+    }
+
+}
---
+++ WpfTestApp/ChartViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+using SciChart.Charting.HistoryManagers;
+using SciChart.Charting.Model.ChartSeries;
+using SciChart.Charting.Visuals.Annotations;
+
+namespace WpfTestApp;
+
+public class ChartViewModel
+{
+    public IZoomHistoryManager ZoomHistoryManager { get; set; }
+
+    public ObservableCollection<IAnnotationViewModel> Annotations { get; set; } = new();
+
+    public string MenuItemText => "Test";
+
+    public ChartViewModel()
+    {
+        ZoomHistoryManager = new ZoomHistoryManager() { HistoryDepth = 10 };
+
+        var textAnnotationViewModel = new TextAnnotationViewModel()
+        {
+            AnnotationCanvas = AnnotationCanvas.YAxis,
+            CanEditText = false,
+            IsHidden = false,
+            Text = "Test",
+            Background = Brushes.Red,
+            CoordinateMode = AnnotationCoordinateMode.Relative,
+            X1 = 0,
+            Y1 = 0
+        };
+        Annotations.Add(textAnnotationViewModel);
+    }
+}
